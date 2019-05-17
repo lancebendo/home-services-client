d78f0d4d337700007e992cae4178e4053201faff
Lance Bendo <lunchbendo@gmail.com>
2019-05-17 00:43:58 -0700

    Nav storybook complete. Didnt include sidenav because its too dumb lol

--- stories/index.js
+++ stories/index.js
@@ -1,26 +1,25 @@
 import React from 'react';
-
 import { BrowserRouter } from 'react-router-dom';
-// import Nav from '../src/components/Nav';
+
+// Nav Component
+import Nav from '../src/components/Nav';
 import NavButton from '../src/components/Nav/NavButton.dumb';
-// import NavLink from './NavLink.dumb';
+import NavLink from '../src/components/Nav/NavLink.dumb';
 
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
-import { linkTo } from '@storybook/addon-links';
 
-import { Button, Welcome } from '@storybook/react/demo';
+const mockPaths = [
+  { name: 'Dashboard', path: '/', icon: 'home' },
+  { name: 'Profile', path: '/profile', icon: 'person' },
+  { name: 'History', path: '/history', icon: 'history' },  
+];
 
-storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
+storiesOf('Nav', module)
+  .add('Nav Smart Component', () => <BrowserRouter><Nav availablePaths={mockPaths} isTest/></BrowserRouter>)
+  .add('NavButton Dumb Component', () => <NavButton label="NavButton" icon="home" onClick={action('NavButton clicked')}/>)
+  .add('NavLink Dumb Component', () => <BrowserRouter><NavLink label="Home" icon="home" path="/" onClick={action('NavLink clicked')} /></BrowserRouter>);
 
-storiesOf('Button', module)
-  .add('with text', () => <Button onClick={action('clicked')}>Hello Button</Button>)
-  .add('with some emoji', () => <Button onClick={action('clicked')}>😀 😎 👍 💯</Button>);
+// storiesOf('Profile', module).add('Nav Smart Component', () => <BrowserRouter><Nav availablePaths={mockPaths} isTest/></BrowserRouter>);
 
-// const mockPaths = [
-//   { name: 'Dashboard', path: '/', icon: 'home' },
-//   { name: 'Profile', path: '/profile', icon: 'person' },
-//   { name: 'History', path: '/history', icon: 'history' },  
-// ];
-
-storiesOf('Nav', module).add('Nav Smart Component', () => <BrowserRouter><NavButton label="NavButton" icon="home" onClick={action('clicked')}/></BrowserRouter>);
+// storiesOf('Shared Components', module).add('Nav Smart Component', () => <BrowserRouter><Nav availablePaths={mockPaths} isTest/></BrowserRouter>);

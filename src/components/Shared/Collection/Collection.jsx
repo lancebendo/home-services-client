456c3be6ccc3d04ffe291e7abf8b33e593fdbf20
Lance Bendo <lunchbendo@gmail.com>
2019-06-30 00:24:56 -0700

    2.0 init

---
+++ src/components/Shared/Collection/Collection.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Collection extends Component {
+    state = { }
+
+    render() {
+      return (
+        <h1>haha</h1>
+      );
+    }
+}
+
+export default Collection;
